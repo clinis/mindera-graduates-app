de5169c7daffa1062c9498adab3b4612f528a413
Dinis <dinisareias@gmail.com>
2018-06-05 23:27:46 +0100

    import data from module

--- App.js
+++ App.js
@@ -4,23 +4,12 @@
  * @flow
  */
 
-import React from 'react';
-import { Button, View, Text, FlatList, ScrollView, TouchableHighlight } from 'react-native';
-import { createStackNavigator, createMaterialTopTabNavigator, createDrawerNavigator } from 'react-navigation';
-import { ListItem } from 'react-native-elements'
-import { Icon } from 'react-native-elements'
-import { YellowBox } from 'react-native'
+import React from 'react'
+import { Button, FlatList, ScrollView, Text, TouchableHighlight, View, YellowBox } from 'react-native'
+import { createDrawerNavigator, createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation'
+import { Icon, ListItem } from 'react-native-elements'
 
-
-const events = [
-  {title: "Open Day '18", days: ['Day 01', 'Day 02', 'Day 03', 'Day 4', 'Day 5']},
-  {title: "Graduate Program", days: ['First day', 'Second day', 'Last day']},
-  {title: 'Meet Mindera Code & Culture', days: ['Day 19', 'Day 20']},
-];
-
-const dayList = ["List 01", "List 02", "List 03", "List 04", "List 05", "List 06", "List 07", "List 08", "List 09", "List 10"]
-
-const listItems = ["Description 01", "Description 02", "Description 03", "Description 04", "Description 05", "Description 06", "Description 07", "Description 08"]
+import { events, dayList, listItems} from './data'
 
 // ignore warnings due to known react native issues
 YellowBox.ignoreWarnings([
---
+++ data.js
@@ -0,0 +1,14 @@
+const events = [
+  {title: "Open Day '18", days: ['Day 01', 'Day 02', 'Day 03', 'Day 4', 'Day 5']},
+  {title: "Graduate Program", days: ['First day', 'Second day', 'Last day']},
+  {title: 'Meet Mindera Code & Culture', days: ['Day 19', 'Day 20']},
+];
+
+const dayList = ["List 01", "List 02", "List 03", "List 04", "List 05", "List 06", "List 07", "List 08", "List 09", "List 10"]
+
+const listItems = ["Description 01", "Description 02", "Description 03", "Description 04", "Description 05", "Description 06", "Description 07", "Description 08"]
+
+
+module.exports = {
+  events, dayList, listItems
+}
